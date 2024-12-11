dc561fe54584e64600b7a99cfd544b58bbd5a786
highblueberry <59508874+highblueberry@users.noreply.github.com>
2024-12-11 20:00:20 +0900

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,17 +1,18 @@
 const Restaurant = require('../models/Restaurant');
 const asyncHandler = require("express-async-handler");
-const mongoose = require("mongoose");
-
 
 // 리뷰 조회
 // GET /review/:id
 const getReviews = asyncHandler(async (req, res) => {
     const restaurant = await Restaurant.findById(req.params.id);
-    if(!restaurant) return res.status(404).json({ message: 'Restaurant not found'});
+    if(!restaurant) return res.status(404).render('404', {
+        title : '404'
+    })
     res.render('review', {
         title : 'Add Reivew',
+        token : req.cookies.token,
         restaurant : restaurant.rest_name,
-        restaurantId : req.params.id
+        restaurantId : req.params.id,
     })
 })
 
@@ -21,38 +22,62 @@ const addReview = asyncHandler(async (req, res) => {
     const { rating, content } = req.body;
     const restaurant = await Restaurant.findById(req.params.id);
 
-    if(!restaurant) return res.status(404).json({ message: 'Restaurant not found' });
+    if(!restaurant) return res.status(404).render('404', {
+        title : '404'
+    });
 
     restaurant.reviews.push({ user_id: req.user.id, rating, content });
     restaurant.total_reviews++;
     restaurant.average_rating = restaurant.reviews.reduce((acc, review) => acc + review.rating, 0) / restaurant.total_reviews;
 
     await restaurant.save();
-    res.status(201).json({ message: 'Review added successfully' });
+    // res.status(201).json({ message: 'Review added successfully' });
+    let url = '/restaurants/' + restaurant.category + '/' + req.params.id;
+    url = decodeURI(url);
+    res.status(201).redirect(url);
 });
+
+
+// 리뷰 수정
+// PUT //review/:id
+const updateReview = asyncHandler(async (req, res) => {
+
+});
+
 
 
 // 리뷰 삭제
 // DELETE //review/:id
 const deleteReview = asyncHandler(async (req, res) => {
-    const reviewId = mongoose.Types.ObjectId(req.query.id);
+    const reviewId = req.params.id
 
-    const restaurant = await Restaurant.findOne({ 'reviews._id': reviewId });
+    const getRestaurantByReviewId = async (reviewId) => {
+        const restaurant = await Restaurant.findOne({ 'reviews._id': reviewId }, { _id: 1 });
+        return restaurant ? restaurant._id : null;
+    };
 
-    console.log(reviewId);
+    const restaurantId = await getRestaurantByReviewId(reviewId);
 
-    if(restaurant) {
-        restaurant.reviews.pull({ _id: reviewId });
-        await restaurant.save();
-    }
+    const result = await Restaurant.findByIdAndUpdate(
+        restaurantId, // 해당 Restaurant의 ID
+        { 
+            $pull: { reviews: { _id: reviewId } }, // reviews 배열에서 특정 _id를 가진 리뷰 제거
+            $inc: { total_reviews: -1 } // total_reviews 값을 1 감소
+        }, 
+        { new: true } // 업데이트된 문서를 반환
+    );
 
+    // average_rating 재계산
+    const restaurant = await Restaurant.findById(restaurantId);
+    restaurant.average_rating = restaurant.reviews.reduce((acc, review) => acc + review.rating, 0) / restaurant.total_reviews;
+    await restaurant.save();
 
-    res.redirect("back");
+    res.redirect('/auth');
 });
-
 
 module.exports = {
     getReviews,
     addReview,
+    updateReview,
     deleteReview
 }
